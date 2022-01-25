5c43fc3556e62f4ea8dab684275fd8555d3179da
Kolya Akulich <6adfeniks@gmail.com>
2022-01-25 15:52:28 +0300

    commit

--- first.go
+++ first.go
@@ -1,20 +1,81 @@
 package main
 
+/*
+Первая часть
+*/
+//import (
+//	"fmt"
+//	"io"
+//	"log"
+//	"net/http"
+//)
+//
+//func Greet(writer io.Writer, name string) {
+//	fmt.Fprintf(writer, "Hello, %s", name)
+//}
+//
+//func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+//	Greet(w, "world")
+//}
+//
+//func main() {
+//	log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler)))
+//}
+
+/*
+Вторая часть
+*/
+
 import (
 	"fmt"
 	"io"
-	"log"
-	"net/http"
+	"os"
+	"time"
 )
 
-func Greet(writer io.Writer, name string) {
-	fmt.Fprintf(writer, "Hello, %s", name)
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+type Sleeper interface {
+	Sleep()
 }
 
-func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
-	Greet(w, "world")
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(b io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(b, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(b, finalWord)
+
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
 }
 
 func main() {
-	log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler)))
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
 }
